e6caab1ed9cd141d1aba1726d12d5305829af67c
刘佳雨 <liujiayu@liujiayus-MacBook-Pro.local>
2020-02-09 14:45:22 -0800

    Add dependencies for pandas and sklearn

--- visualization/views.py
+++ visualization/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+import pandas as pd
+from sklearn import linear_model
+import os
+from Assignment4 import settings
 
 # Create your views here.
 def __test__(request):
-    return render(request, 'testpage.html')
+    df = pd.read_csv(os.path.join(settings.BASE_DIR, 'student-mat.csv'))
+    print(df.columns)
+
+    reg = linear_model.LinearRegression()
+    reg.fit([[0, 0], [1, 1], [2, 2]], [0, 1, 2])
+    print(reg.coef_)
+
+    return render(request, 'testpage.html') 
